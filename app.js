d690ba13666aabfffe1694bd48c044e12e7c8f6c
efrainpa <91482821+efrainpa@users.noreply.github.com>
2026-09-15 12:17:56 -0600

    Update app.js

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ function cargaDias(segundos, minutos, horas) {
 setInterval(cargarSegundo, 1000);
 
 function play(){
-    var audio = new Audio('musica/Cuando empezare a vivir (audio).mp3');
+    var audio = new Audio('musica/misho2.mp3');
     audio.play();
     var boton = document.getElementById('btn-play');
     boton.disabled=true;
